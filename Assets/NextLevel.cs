16be64131f12fc23dd58c33d6c0f5a9a9317315d
charmant6 <85350693+charmant6@users.noreply.github.com>
2023-01-15 17:10:34 +0300

    Update NextLevel.cs

--- Assets/NextLevel.cs
+++ Assets/NextLevel.cs
@@ -10,6 +10,7 @@ public class NextLevel : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+            Debug.Log("error");
         }
     }
 }
